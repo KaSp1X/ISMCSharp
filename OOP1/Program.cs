557f309131a5f1389a605c7033e908300e1aace2
KaSp1X <romankasp1x@gmail.com>
2019-05-01 19:24:40 +0300

    Добавлена библиотека OOPLib1 и консольный проект OOP1(Task04)

---
+++ OOP1/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPLib1;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane plane1 = new Airplane();
+            PrintAirplane(plane1);
+            Airplane[] planes1 = ReadAirplaneArray();
+            int largestTravel, leastTravel;
+            GetAirplaneInfo(planes1, out largestTravel, out leastTravel);
+            PrintAirplanes(planes1);
+            SortAirplanesByDate(ref planes1);
+            PrintAirplanes(planes1);
+            SortAirplanesByTotalTime(ref planes1);
+            PrintAirplanes(planes1);
+            Console.ReadKey();
+        }
+        static Airplane[] ReadAirplaneArray()
+        {
+            bool check = false;
+            int CountOfElements;
+            do
+            {
+                Console.Write("Введите количество рейсов: ");
+                check = int.TryParse(Console.ReadLine(), out CountOfElements);
+                if (!check) Console.WriteLine("Данные введено неверно!");
+            }
+            while (!check);
+            Airplane[] airplanes = new Airplane[CountOfElements];
+            string startCity, finishCity;
+            Date startDate, finishDate;
+            int Year, Month, Day, Hours, Minutes;
+            for(int i = 0; i < airplanes.Length; i++)
+            {
+                Console.WriteLine("--------------------------------|");
+                Console.WriteLine($"Введите данные рейса номер {i,3}! |");
+                Console.WriteLine( "Примечание: при ошибке ввода    |\n" +
+                                   "данных, поле получит стандартное|\n" +
+                                   "значение!                       |");
+                Console.Write("Введите город отправления       |  ");
+                startCity = Console.ReadLine();
+                Console.Write("Введите город прибытия          |  ");
+                finishCity = Console.ReadLine();
+                Console.WriteLine( "Введите дату отправления!       |");
+                Console.WriteLine("Примечание: вводить все числом! |");
+                Console.Write("Введите год                     |  ");
+                int.TryParse(Console.ReadLine(), out Year);
+                Console.Write("Введите месяц                   |  ");
+                int.TryParse(Console.ReadLine(), out Month);
+                Console.Write("Введите день месяца             |  ");
+                int.TryParse(Console.ReadLine(), out Day);
+                Console.Write("Введите час                     |  ");
+                int.TryParse(Console.ReadLine(), out Hours);
+                Console.Write("Введите минуты                  |  ");
+                int.TryParse(Console.ReadLine(), out Minutes);
+                startDate = new Date(Year, Month, Day, Hours, Minutes);
+                Console.WriteLine( "Введите дату прыбитыя!          |");
+                Console.WriteLine("Примечание: вводить все числом! |");
+                Console.Write("Введите год                     |  ");
+                int.TryParse(Console.ReadLine(), out Year);
+                Console.Write("Введите месяц                   |  ");
+                int.TryParse(Console.ReadLine(), out Month);
+                Console.Write("Введите день месяца             |  ");
+                int.TryParse(Console.ReadLine(), out Day);
+                Console.Write("Введите час                     |  ");
+                int.TryParse(Console.ReadLine(), out Hours);
+                Console.Write("Введите минуты                  |  ");
+                int.TryParse(Console.ReadLine(), out Minutes);
+                finishDate = new Date(Year, Month, Day, Hours, Minutes);
+                Console.WriteLine("--------------------------------|");
+                airplanes[i] = new Airplane(startCity, finishCity, startDate, finishDate);
+            }
+            return airplanes;
+        }
+        static void PrintAirplane(Airplane airplane)
+        {
+            string startCity = airplane.GetStartCity();
+            string finishCity = airplane.GetFinishCity();
+            string startDate = airplane.GetStartDate().GetYear()+"."+ airplane.GetStartDate().GetMonth() + "."+ airplane.GetStartDate().GetDay() + " "+ airplane.GetStartDate().GetHours() + ":"+ airplane.GetStartDate().GetMinutes();
+            string finishDate = airplane.GetFinishDate().GetYear()+"."+ airplane.GetFinishDate().GetMonth() + "."+ airplane.GetFinishDate().GetDay() + " "+ airplane.GetFinishDate().GetHours() + ":"+ airplane.GetFinishDate().GetMinutes();
+            string arrivingToday = "";
+            if (airplane.IsArrivingToday()) arrivingToday = "Да"; else arrivingToday = "Нет";
+            string totalTime = airplane.GetTotalTime().ToString();
+            Console.WriteLine("--------------------------------|");
+            Console.WriteLine($"Стартовый город                 |  {startCity}");
+            Console.WriteLine($"Конечный город                  |  {finishCity}");
+            Console.WriteLine($"Дата вылета                     |  {startDate}");
+            Console.WriteLine($"Дата прибытия                   |  {finishDate}");
+            Console.WriteLine($"Прибытие в день когда и вылет   |  {arrivingToday}");
+            Console.WriteLine($"Общее время полёта              |  {totalTime}");
+            Console.WriteLine("--------------------------------|");
+        }
+
+        static void PrintAirplanes(Airplane[] airplanes)
+        {
+            string startCity, finishCity, startDate, finishDate, arrivingToday, totalTime;
+            for (int i = 0; i < airplanes.Length; i++)
+            {
+                startCity = airplanes[i].GetStartCity();
+                finishCity = airplanes[i].GetFinishCity();
+                startDate = airplanes[i].GetStartDate().GetYear() + "." + airplanes[i].GetStartDate().GetMonth() + "." + airplanes[i].GetStartDate().GetDay() + " " + airplanes[i].GetStartDate().GetHours() + ":" + airplanes[i].GetStartDate().GetMinutes();
+                finishDate = airplanes[i].GetFinishDate().GetYear() + "." + airplanes[i].GetFinishDate().GetMonth() + "." + airplanes[i].GetFinishDate().GetDay() + " " + airplanes[i].GetFinishDate().GetHours() + ":" + airplanes[i].GetFinishDate().GetMinutes();
+                if (airplanes[i].IsArrivingToday()) arrivingToday = "Да"; else arrivingToday = "Нет";
+                totalTime = airplanes[i].GetTotalTime().ToString();
+                Console.WriteLine("--------------------------------|");
+                Console.WriteLine($"Номер самолёта                  |  {i}");
+                Console.WriteLine($"Стартовый город                 |  {startCity}");
+                Console.WriteLine($"Конечный город                  |  {finishCity}");
+                Console.WriteLine($"Дата вылета                     |  {startDate}");
+                Console.WriteLine($"Дата прибытия                   |  {finishDate}");
+                Console.WriteLine($"Прибытие в день когда и вылет   |  {arrivingToday}");
+                Console.WriteLine($"Общее время полёта              |  {totalTime}");
+                Console.WriteLine("--------------------------------|");
+            } 
+        }
+
+        static void GetAirplaneInfo(Airplane[] airplanes, out int largestTravel, out int leastTravel)
+        {
+            largestTravel = int.MinValue;
+            leastTravel = int.MinValue;
+            for (int i = 0; i < airplanes.Length; i++)
+            {
+                if (airplanes[i].GetTotalTime() > largestTravel) largestTravel = airplanes[i].GetTotalTime();
+                if (airplanes[i].GetTotalTime() < leastTravel) leastTravel = airplanes[i].GetTotalTime();
+            }
+        }
+        
+        static void tmpTime(Date date, out int Year, out int Month, out int Day, out int Hours, out int Minutes)
+        {
+            Year = date.GetYear();
+            Month = date.GetMonth();
+            Day = date.GetDay();
+            Hours = date.GetHours();
+            Minutes = date.GetMinutes();
+        }
+        static void SortAirplanesByDate(ref Airplane[] airplanes)
+        {
+            Date tmpDate;
+            DateTime airplaneTime_1, airplaneTime_2;
+            int tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes;
+            string tmpStartCity, tmpFinishCity;
+            for (int i = 0; i < airplanes.Length - 1; i++)
+            {
+                tmpTime(airplanes[i].GetStartDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                airplaneTime_1 = new DateTime(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes, 0);
+                for (int j = i + 1; j < airplanes.Length; j++)
+                {
+                    tmpTime(airplanes[j].GetStartDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                    airplaneTime_2 = new DateTime(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes, 0);
+                    if (airplaneTime_1 < airplaneTime_2)
+                    {
+                        tmpStartCity = airplanes[i].GetStartCity();
+                        airplanes[i].SetStartCity(airplanes[j].GetStartCity());
+                        airplanes[j].SetStartCity(tmpStartCity);
+
+                        tmpFinishCity = airplanes[i].GetFinishCity();
+                        airplanes[i].SetFinishCity(airplanes[j].GetFinishCity());
+                        airplanes[j].SetFinishCity(tmpFinishCity);
+
+                        tmpDate = airplanes[i].GetStartDate();
+                        tmpTime(airplanes[i].GetStartDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[i].SetStartDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                        tmpTime(tmpDate, out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[j].SetStartDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+
+                        tmpDate = airplanes[i].GetFinishDate();
+                        tmpTime(airplanes[i].GetFinishDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[i].SetFinishDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                        tmpTime(tmpDate, out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[j].SetFinishDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                    }
+                }
+            }
+        }
+
+        static void SortAirplanesByTotalTime(ref Airplane[] airplanes)
+        {
+            Date tmpDate;
+            int travelTime_1, travelTime_2;
+            int tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes;
+            string tmpStartCity, tmpFinishCity;
+            for (int i = 0; i < airplanes.Length - 1; i++)
+            {
+                travelTime_1 = airplanes[i].GetTotalTime();
+                for (int j = i + 1; j < airplanes.Length; j++)
+                {
+                    travelTime_2 = airplanes[j].GetTotalTime();
+                    if (travelTime_1 > travelTime_2)
+                    {
+                        tmpStartCity = airplanes[i].GetStartCity();
+                        airplanes[i].SetStartCity(airplanes[j].GetStartCity());
+                        airplanes[j].SetStartCity(tmpStartCity);
+
+                        tmpFinishCity = airplanes[i].GetFinishCity();
+                        airplanes[i].SetFinishCity(airplanes[j].GetFinishCity());
+                        airplanes[j].SetFinishCity(tmpFinishCity);
+
+                        tmpDate = airplanes[i].GetStartDate();
+                        tmpTime(airplanes[i].GetStartDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[i].SetStartDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                        tmpTime(tmpDate, out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[j].SetStartDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+
+                        tmpDate = airplanes[i].GetFinishDate();
+                        tmpTime(airplanes[i].GetFinishDate(), out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[i].SetFinishDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                        tmpTime(tmpDate, out tmpYear, out tmpMonth, out tmpDay, out tmpHours, out tmpMinutes);
+                        airplanes[j].SetFinishDate(tmpYear, tmpMonth, tmpDay, tmpHours, tmpMinutes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OOPLib1/OOPLib1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLib1
+{
+    public class Airplane
+    {
+        protected string StartCity;
+        protected string FinishCity;
+        protected Date StartDate;
+        protected Date FinishDate;
+
+        /// Стартовый город по умолчанию - Киев, Украина
+        /// Конечный город по умолчанию - Оттава, Канада
+        /// Время полёта ~9 часов
+        /// Источник: http://time-fly.ru/skolko-letet-do-kanady-iz-kieva
+
+        public Airplane()
+        {
+            StartCity = "Киев";
+            FinishCity = "Оттава";
+            StartDate = new Date();
+            FinishDate = new Date(StartDate);
+            FinishDate.SetHours(FinishDate.GetHours() + 9);
+        }
+        public Airplane(string startcity, string finishcity, Date startdate, Date finishdate)
+        {
+            StartCity = SetStartCity(startcity);
+            FinishCity = SetFinishCity(finishcity);
+            StartDate = new Date(startdate);
+            FinishDate = new Date(finishdate);
+        }
+        public Airplane(string startcity, Date startdate, Date finishdate)
+        {
+            StartCity = startcity;
+            FinishCity = "Оттава";
+            StartDate = new Date(startdate);
+            FinishDate = new Date(finishdate);
+        }
+        public Airplane(string finishcity, Date startdate)
+        {
+            StartCity = "Киев";
+            FinishCity = finishcity;
+            StartDate = new Date(startdate);
+            FinishDate = new Date(startdate);
+            FinishDate.SetHours(FinishDate.GetHours() + 9);
+        }
+        public Airplane(Airplane airplane)
+        {
+            StartCity = airplane.StartCity;
+            FinishCity = airplane.FinishCity;
+            StartDate = new Date(airplane.StartDate);
+            FinishDate = new Date(airplane.FinishDate);
+        }
+        public string SetStartCity(string startcity)
+        {
+            if (startcity.Length > 0) StartCity = startcity;
+            else
+                StartCity = "Киев";
+            return StartCity;
+        }
+
+        public string GetStartCity()
+        {
+            return StartCity;
+        }
+        public string SetFinishCity(string finishcity)
+        {
+            if (finishcity.Length > 0 && finishcity != StartCity) FinishCity = finishcity;
+            else
+                FinishCity = "Оттава";
+            return FinishCity;
+        }
+        public string GetFinishCity()
+        {
+            return FinishCity;
+        }
+
+        public void SetStartDate(int year, int month, int day, int hours, int minutes)
+        {
+            StartDate.SetYear(year);
+            StartDate.SetMonth(month);
+            StartDate.SetDay(day);
+            StartDate.SetHours(hours);
+            StartDate.SetMinutes(minutes);
+        }
+
+        public Date GetStartDate()
+        {
+            return new Date(StartDate);
+        }
+        public void SetFinishDate(int year, int month, int day, int hours, int minutes)
+        {
+            FinishDate.SetYear(year);
+            FinishDate.SetMonth(month);
+            FinishDate.SetDay(day);
+            FinishDate.SetHours(hours);
+            FinishDate.SetMinutes(minutes);
+        }
+
+        public Date GetFinishDate()
+        {
+            return new Date(FinishDate);
+        }
+
+        public int GetTotalTime()
+        {
+            int TimeInMinutes;
+            DateTime startDate = new DateTime(StartDate.GetYear(), StartDate.GetMonth(), StartDate.GetDay(), StartDate.GetHours(), StartDate.GetMinutes(), 0);
+            DateTime finishDate = new DateTime(FinishDate.GetYear(), FinishDate.GetMonth(), FinishDate.GetDay(), FinishDate.GetHours(), FinishDate.GetMinutes(), 0);
+            if (startDate > finishDate) return 0;
+            TimeSpan totalTime = finishDate - startDate;
+            TimeInMinutes = (int)totalTime.TotalMinutes;
+            return TimeInMinutes;
+        }
+
+        public bool IsArrivingToday()
+        {
+            if (StartDate.GetYear() == FinishDate.GetYear() && StartDate.GetMonth() == FinishDate.GetMonth() && StartDate.GetDay() == FinishDate.GetDay())
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    public class Date
+    {
+        protected int Year;
+        protected int Month;
+        protected int Day;
+        protected int Hours;
+        protected int Minutes;
+
+        public Date()
+        {
+            Year = DateTime.Now.Year;
+            Month = DateTime.Now.Month;
+            Day = DateTime.Now.Day;
+            Hours = DateTime.Now.Hour;
+            Minutes = DateTime.Now.Minute;
+        }
+
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            Year = SetYear(year);
+            Month = SetMonth(month);
+            Day = SetDay(day);
+            Hours = SetHours(hours);
+            Minutes = SetMinutes(minutes);
+        }
+
+        public Date(int year, int month, int day, int hours)
+        {
+            Year = SetYear(year);
+            Month = SetMonth(month);
+            Day = SetDay(day);
+            Hours = SetHours(hours);
+            Minutes = 0;
+        }
+
+        public Date(int month, int day, int hours)
+        {
+            Year = DateTime.Now.Year;
+            Month = SetMonth(month);
+            Day = SetDay(day);
+            Hours = SetHours(hours);
+            Minutes = 0;
+        }
+
+        public Date(int day, int hours)
+        {
+            Year = DateTime.Now.Year;
+            Month = DateTime.Now.Month;
+            Day = SetDay(day);
+            Hours = SetHours(hours);
+            Minutes = 0;
+        }
+
+        public Date(int hours)
+        {
+            Year = DateTime.Now.Year;
+            Month = DateTime.Now.Month;
+            Day = DateTime.Now.Day;
+            Hours = SetHours(hours);
+            Minutes = 0;
+        }
+
+        public Date(Date date)
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+            Hours = date.Hours;
+            Minutes = date.Minutes;
+        }
+
+        public int SetYear(int year)
+        {
+            if (1900 <= year && year < 3000) Year = year;
+            else Year = DateTime.Now.Year;
+            return Year;
+        }
+
+        public int GetYear()
+        {
+            return Year;
+        }
+
+        public int SetMonth(int month)
+        {
+            if (1 <= month && month <= 12) Month = month;
+            else Month = DateTime.Now.Month;
+            while (Month > 12) { Month -= 12; Year++; }
+            return Month;
+        }
+
+        public int GetMonth()
+        {
+            return Month;
+        }
+
+        public int SetDay(int day)
+        {
+            if (1 <= Day && Day <= DateTime.DaysInMonth(Year, Month)) Day = day;
+            else Day = DateTime.Now.Day;
+            while (Day > DateTime.DaysInMonth(Year, Month)) { Day -= DateTime.DaysInMonth(Year, Month); SetMonth(Month + 1); }
+            return Day;
+        }
+
+        public int GetDay()
+        {
+            return Day;
+        }
+
+        public int SetHours(int hours)
+        {
+            if (0 <= Hours && Hours <= 23) Hours = hours;
+            else Hours = DateTime.Now.Hour;
+            while (Hours > 23) { Hours -= 24; SetDay(Day + 1); }
+            return Hours;
+        }
+
+        public int GetHours()
+        {
+            return Hours;
+        }
+
+        public int SetMinutes(int minutes)
+        {
+            if (0 <= Minutes && Minutes <= 59) Minutes = minutes;
+            else Minutes = DateTime.Now.Minute;
+            while (Minutes > 59) { Minutes -= 60; SetHours(Hours + 1); }
+            return Minutes;
+        }
+
+        public int GetMinutes()
+        {
+            return Minutes;
+        }
+    }
+}
